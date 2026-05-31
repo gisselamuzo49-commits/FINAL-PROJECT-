b50ba2471758eafef43453102a9b42508a113702
Gissel <gisselamuzo49@gmail.com>
2026-05-31 10:38:57 -0500

    fix: add missing Ubuntu security group and user data

--- infra/qa/main.tf
+++ infra/qa/main.tf
@@ -17,12 +17,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# --- NUEVO: 1. EL GUARDIA DE SEGURIDAD ---
+# --- 1. EL GUARDIA DE SEGURIDAD (¡Este bloque faltaba!) ---
 resource "aws_security_group" "qa_sg" {
   name        = "qa_security_group"
   description = "Permitir trafico web y backend"
 
-  # Abrir Puerto 80 para la Web (HTTP)
   ingress {
     from_port   = 80
     to_port     = 80
@@ -30,7 +29,6 @@ resource "aws_security_group" "qa_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Abrir Puerto 8080 para tu futuro backend en Java (Spring Boot)
   ingress {
     from_port   = 8080
     to_port     = 8080
@@ -38,7 +36,6 @@ resource "aws_security_group" "qa_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Permitir que el servidor navegue a internet para descargar cosas
   egress {
     from_port   = 0
     to_port     = 0
@@ -52,19 +49,16 @@ resource "aws_instance" "backend_qa_server" {
   ami           = "ami-0c7217cdde317cfec" 
   instance_type = "t2.micro"              
   
-  # Asignarle el guardia de seguridad a este servidor
   vpc_security_group_ids = [aws_security_group.qa_sg.id]
 
-  # NUEVO: Instrucciones de encendido automático
+  user_data_replace_on_change = true
+
   user_data = <<-EOF
               #!/bin/bash
-              # Actualizar el sistema
-              yum update -y
-              # Instalar Java 17 y un servidor web básico
-              yum install -y java-17-amazon-corretto httpd
-              systemctl start httpd
-              systemctl enable httpd
-              # Crear una página de prueba
+              apt-get update -y
+              apt-get install -y openjdk-17-jdk apache2
+              systemctl start apache2
+              systemctl enable apache2
               echo "<h1>¡Felicidades Gissela! Tu servidor automatizado con GitOps esta vivo.</h1><p>Java ya esta instalado y listo para el backend de tu Sistema de Pasantias.</p>" > /var/www/html/index.html
               EOF
 
