6e2410ce68494b734a99d69c8585627f97f7a493
Gissel <gisselamuzo49@gmail.com>
2026-05-31 10:55:35 -0500

    feat: infraestructura para alojar el frontend en S3 Web

--- infra/qa/main.tf
+++ infra/qa/main.tf
@@ -68,3 +68,59 @@ resource "aws_instance" "backend_qa_server" {
     Project     = "Sistema de Pasantias"
   }
 }
+
+# =========================================================
+# --- 3. EL ALOJAMIENTO PARA TU FRONTEND (S3 WEB) ---
+# =========================================================
+
+# 3.1 Crear el bucket (el nombre debe ser único en el mundo)
+resource "aws_s3_bucket" "frontend_bucket" {
+  bucket = "frontend-pasantias-gisse-2026" # <-- PERSONALIZA ESTE NOMBRE
+}
+
+# 3.2 Configurar el bucket para que funcione como un sitio web
+resource "aws_s3_bucket_website_configuration" "frontend_website" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = "index.html" # Truco vital para que React funcione sin errores 404
+  }
+}
+
+# 3.3 Apagar el candado de seguridad para que internet pueda ver la página
+resource "aws_s3_bucket_public_access_block" "frontend_public_access" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+# 3.4 Crear la regla que permite a cualquier persona leer tu web
+resource "aws_s3_bucket_policy" "frontend_policy" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+  depends_on = [aws_s3_bucket_public_access_block.frontend_public_access]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.frontend_bucket.arn}/*"
+      },
+    ]
+  })
+}
+
+# 3.5 ¡Imprimir el link público al final!
+output "enlace_frontend" {
+  description = "El link público para ver tu frontend en internet"
+  value       = "http://${aws_s3_bucket.frontend_bucket.bucket}.s3-website-us-east-1.amazonaws.com"
+}
